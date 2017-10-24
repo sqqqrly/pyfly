414704cb85380685a2e57b803073ac00df07eefe
David Ohlemacher <ohlemacher@us.ibm.com>
2017-10-24 16:25:26 -0400

    configure container for py. Add die.

--- configure_container.sh
+++ configure_container.sh
@@ -14,7 +14,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y \
     python3.4 \
     python3-pip || die "apt install failed"
 DEBIAN_FRONTEND=noninteractive apt-get clean -y || die "apt install failed"
-pip3 install -r requirements.txt || die "pyfly pip requirements failed"
+pip3 install -r pyfly/requirements.txt || die "pyfly pip requirements failed"
 pip3 install pytest-flask || die "pytest-flask install failed"
 
 echo "== Configuring container (end)"
