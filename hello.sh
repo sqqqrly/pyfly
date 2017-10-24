a88691212d56bc51726609ad1abb53e6e2e6822b
David Ohlemacher <ohlemacher@us.ibm.com>
2017-10-24 11:51:41 -0400

    configure container for py

--- hello.sh
+++ hello.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Configuring container (begin)"
-apt install python3
+sudo apt-get update
+sudo apt-get install python3.4
 apt install python3-pip
 pushd pyfly-repo
 pip3 install -r requirements.txt
