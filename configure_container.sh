acba2f5db6772ddf5a3c37f233f437cfec82ab35
David Ohlemacher <ohlemacher@us.ibm.com>
2017-10-24 16:23:04 -0400

    configure container for py

--- configure_container.sh
+++ configure_container.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+function die {
+    local msg=“$1”
+    (>&2 echo Fatal: “$msg”)  # Subshell avoids interactions with other redirections
+    exit 1
+}
+
+
 echo "== Configuring container (begin)"
 
-DEBIAN_FRONTEND=noninteractive apt-get update -y &&\
-	DEBIAN_FRONTEND=noninteractive apt-get install -y\
-	python3.4 \
-	python3-pip &&\
-	DEBIAN_FRONTEND=noninteractive apt-get clean &&
-pip3 install -r requirements.txt &&
-pip3 install pytest-flask &&
-
-pushd pyfly || (2>&1 echo "Error: configure_container failed"; exit 1)
+DEBIAN_FRONTEND=noninteractive apt-get update -y || die "apt update failed"
+DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    python3.4 \
+    python3-pip || die "apt install failed"
+DEBIAN_FRONTEND=noninteractive apt-get clean -y || die "apt install failed"
+pip3 install -r requirements.txt || die "pyfly pip requirements failed"
+pip3 install pytest-flask || die "pytest-flask install failed"
 
 echo "== Configuring container (end)"
