31ac0094b85cb005491ebc50cd2b7880ac1bbaa7
David Ohlemacher <ohlemacher@us.ibm.com>
2017-10-24 15:16:14 -0400

    configure container for py

--- configure_container.sh
+++ configure_container.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 echo "== Configuring container (begin)"
+
 DEBIAN_FRONTEND=noninteractive apt-get update -y &&\
 	DEBIAN_FRONTEND=noninteractive apt-get install -y\
 	python3.4 \
 	python3-pip &&\
-	DEBIAN_FRONTEND=noninteractive apt-get clean
-pushd pyfly-repo
-pip3 install -r requirements.txt
-pip3 install pytest-flask
+	DEBIAN_FRONTEND=noninteractive apt-get clean &&
+pip3 install -r requirements.txt &&
+pip3 install pytest-flask &&
+
+pushd pyfly || (2>&1 echo "Error: configure_container failed")
+
 echo "== Configuring container (end)"
